e4f58172039acce6ff33296d22c694b38ab627eb
DiogodePaula <depaulatrabalhos@gmail.com>
2020-06-21 17:27:10 -0300

    ainda falta o delete funcionar

--- index.js
+++ index.js
@@ -6,13 +6,38 @@ server.use(express.json());
 
 const list = [];
 
+function verify(req,res,next) {
+
+    const {title, content, date, hour} = req.body;
+
+    if(!title){
+        return res.json({
+            error: "title is required"
+        });      
+    }else if (!content){
+        return res.json({
+            error: "content is required"
+        });
+    }else if (!date){
+        return res.json({
+            error: "date is required"
+        });
+    }else if (!hour){
+        return res.json({
+            error: "hour is required"
+        });
+    }
+
+    next();
+}
+
 server.get("/", (req,res) =>{
     return res.json({
         result: "Welcome to Notepad"
     });
 });
 
-server.post("/list", (req, res) =>{
+server.post("/list",verify, (req, res) =>{
     const {title, content, date, hour} = req.body;
 
     const note = {title, content, date, hour};
@@ -34,7 +59,7 @@ server.get("/list/:id", (req, res) =>{
     });
 });
 
-server.put("/list/:id", (req,res) =>{
+server.put("/list/:id",verify, (req,res) =>{
     const {title, content, date, hour} = req.body;
     const {id} = req.params;
     const note = {title, content, date, hour};
@@ -47,15 +72,15 @@ server.put("/list/:id", (req,res) =>{
     });
 });
 
-server.delete("/list/:id",(req,res)=>{
-    const {title, content, date, hour} = req.body;
+server.delete("/list/:id", (req,res)=>{
+
     const {id} = req.params;
-    const note = {title, content, date, hour};
-    list[id] = note;
+    list.find(id);
 
     return res.json({
-        result: "deleted"
-    })
-})
+        result: "deleted",
+        
+    });
+});
 
 server.listen(3000);
